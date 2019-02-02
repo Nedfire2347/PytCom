892a08d0725746a9fa0e6e13c7744316f2392ce3
Temperance <adulorier@gmail.com>
2019-02-02 14:52:14 +0100

    optimize client

--- client.py
+++ client.py
@@ -7,24 +7,23 @@ import sys
 hote = "localhost"
 port = 15555
 
-global Run
-global Con
 Con = False
-Run = True
-global socket
+socket = None
 
 def Exit():
-    socket.close()
-    print ("Close")
-    sys.exit()
+    if socket is not None:
+        socket.close()
+    print("Close")
+    sys.exit(0)
 
-def connect(host, por) :
+def connect(hote=hote, port=port) :
     global Con
     global socket
+
     try:
         socket = sockett.socket(sockett.AF_INET, sockett.SOCK_STREAM)
         socket.setsockopt(sockett.SOL_SOCKET, sockett.SO_REUSEADDR, 1)
-        socket.connect((hote, por))
+        socket.connect((hote, port))
         Con = True
         print ("Connection on {}".format(port))
 
@@ -34,42 +33,71 @@ def connect(host, por) :
         threadR.start()
 
     except:
-        print ('erreur, Connextion immpossible')
-        Exit()
+        socket.close()
+        print ('erreur, Connexion immpossible')
 
-
-##Tread recive## 
-class myThreadRevived (threading.Thread): 
+##Tread recive##
+class myThreadRevived (threading.Thread):
     def __init__(self, thread):
         threading.Thread.__init__(self)
+        self.daemon = True
         self.thread = thread
     def run(self):
-        response = self.thread.recv(255)
+        print("ok")
+        try:
+            response = self.thread.recv(255)
+        except KeyboardInterrupt:
+            Exit()
         if (response.decode()) == "/stop":
-            global Con 
+            global Con
             Con = False
-            print (' Connextion Fermer')
+            print (" Connexion Fermée")
             Exit()
         print(response.decode())
 
-while Run:
-    imp = input()
-    if Con == False :
-        if imp == "/connect":
-            connect(hote, port)
-        else :
-            if imp == "/close":
-                Run = False
-                print("fermeture")
-            else :
-                print ("cmd inconu")
+def send(inp):
+    global Con
+
+    try:
+        socket.send(str.encode(inp))
+    except:
+        print("erreur, Connexion disparue")
+        Con = False
+
+def close():
+    global Con
+    global Run
+
     if Con == True:
-        if imp == "/close":
-            Run = False
-            print("fermeture")
-            imp = "/stop"
-        try:
-            socket.send(str.encode(imp))
-        except:
-            print ('erreur, Connextion disparu')
-            Run = False 
+        send("/stop")
+    Con = False
+    Run = False
+
+def loop_connection():
+    global Con
+
+    while Con:
+        inp = input()
+        if inp == "/close":
+            close()
+        else:
+            send(inp)
+Run = True
+while Run:
+    try:
+        inp = input()
+    except (KeyboardInterrupt, SystemExit):
+        raise
+
+    if inp == "/close":
+        close()
+        Run = False
+    elif inp == "/connect":
+        connect()
+        if Con:
+            loop_connection()
+    else:
+        print("cmd inconnue")
+
+
+
